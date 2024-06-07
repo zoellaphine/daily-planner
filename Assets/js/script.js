1f3bc65798a77b4e4581453cd6ca81dd310cce0f
katzeundmais <zoellaphine@gmail.com>
2024-06-07 19:14:14 -0400

    finished functionality

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+const saveBtn = document.querySelector('.container-fluid');
+
 $(function () {
   let today = new Date();
 
@@ -13,18 +15,23 @@ $(function () {
     timeBlocks.push(document.getElementById('hour-' + j));
   }
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // Tried to do just button event delegation but because all of the buttons
+  // are in separate divs I wasn't sure how to approach it. Event listens for 
+  // *any* click on the page, but will only do anything for a button press.
+  // Gets id of the div where the button is and sets that as the id in localStorage.
+  saveBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (event.target.classList.contains('btn')) {
+      let block = event.target.parentNode.id;
+      let content = document.getElementById(block).children[1].value;
+      localStorage.setItem(block, content);
+    }
+  })
 
   let currentTime = today.getHours();
   // As we iterate through the times 9-17, have l represent which timeblock is
   // to be adjusted and compare the iteration to the currentTime.
   let l = 0;
-  console.log(currentTime);
   for (let k = 9; k < 18; k++) {
     if (k == currentTime) {
       timeBlocks[l].classList.add('present');
@@ -36,9 +43,16 @@ $(function () {
     l++;
   }
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // Two iterations through the correct times to pull the values from localStorage.
+  for (let m = 9; m < 13; m++) {
+    let id = 'hour-' + m;
+    document.getElementById(id).children[1].value = localStorage.getItem(id);
+  }
+
+  for (let n = 1; n < 6; n++) {
+    let id = 'hour-' + n;
+    document.getElementById(id).children[1].value = localStorage.getItem(id);
+  }
 
   //Add current date to the header
   const element = document.getElementById('currentDay');
